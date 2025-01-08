f8f561c7436a1c5b954bab8873cebe6447c0b75c
VicFlores11 <vicflores2211@gmail.com>
2025-01-08 17:16:12 -0600

    feat:[ADD] styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Home } from './components';
-import { Navbar } from './components/shared';
 
 const HomePage = () => {
   return (
     <>
-      <Navbar />
-
       <Home />
     </>
   );
