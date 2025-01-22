1af6c4b8164b5f9964b7d0247525bfe7180077ae
VicFlores11 <vicflores2211@gmail.com>
2025-01-21 20:55:56 -0600

    fix: correct reservation ID query parameter formatting in API request

--- utils/helpers/findReservationById.ts
+++ utils/helpers/findReservationById.ts
@@ -3,7 +3,7 @@ import checkinAPI from '../config/axiosConfig';
 export const findReservationById = async (id: string) => {
   try {
     const getReservation = await checkinAPI.get(
-      `/guests/reservation?reservation_id =${id}`
+      `/guests/reservation?reservation_id=${id}`
     );
     return getReservation.data;
   } catch (error) {
