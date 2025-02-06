495f1240341cc88d04ca0dee87107820d5752e27
Vic Flores <vicflores2211@gmail.com>
2025-02-06 11:50:23 -0600

    feat: add X-API-Key header to axios configuration for enhanced security

--- utils/config/axiosConfig.ts
+++ utils/config/axiosConfig.ts
@@ -4,6 +4,7 @@ const checkinAPI = axios.create({
   baseURL: 'https://checkinbackend-production.up.railway.app/api/v1',
   headers: {
     'Content-Type': 'application/json',
+    'X-API-Key': process.env.NEXT_PUBLIC_X_API_Key,
     Accept: 'application/json',
   },
 });
