0721c248dcb1903469a95c62875cc06b2be73ed8
Vic Flores <vicflores2211@gmail.com>
2025-03-05 14:53:37 -0600

    feat: add metadata for Home page to enhance SEO and improve page title

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Home } from './components';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Checkin - Home',
+  description: 'Checkin - Home',
+};
 
 const HomePage = () => {
   return <Home />;
